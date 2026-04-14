35497adcc7fef34ff4af408a3ca246989840e8fa
haitam <merrashaitam@gmail.com>
2026-04-14 17:46:11 +0200

    Added Netlify server adapter for Keystatic GitHub login

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import keystatic from '@keystatic/astro';
+import netlify from '@astrojs/netlify'; // <-- We added this
 
 export default defineConfig({
   site: 'https://drk-onero.me',
+  adapter: netlify(), // <-- And this
   integrations: [
     react(),
     keystatic()
