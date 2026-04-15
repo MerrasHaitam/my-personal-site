e22fe11641bc00ba983031bc16bfc38907e8d59d
haitam <merrashaitam@gmail.com>
2026-04-15 18:19:24 +0200

    Swapped Netlify for Vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import keystatic from '@keystatic/astro';
-import netlify from '@astrojs/netlify'; 
+import vercel from '@astrojs/vercel/serverless'; 
 
 export default defineConfig({
   site: 'https://drk-onero.me',
-  output: 'static', // 
-  adapter: netlify(), 
+  output: 'static', 
+  adapter: vercel(), 
   integrations: [
     react(),
     keystatic()
