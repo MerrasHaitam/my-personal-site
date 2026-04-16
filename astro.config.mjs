dc4c8e0fa4d93e1d4f61898beaf41c6427fcdfa8
haitam <merrashaitam@gmail.com>
2026-04-16 17:15:14 +0200

    Installed official Markdoc translator

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,11 @@ import react from '@astrojs/react';
 import keystatic from '@keystatic/astro';
 import vercel from '@astrojs/vercel'; // <--- Removed the /serverless part
 
+import markdoc from '@astrojs/markdoc';
+
 export default defineConfig({
   site: 'https://drk-onero.me',
   output: 'static', 
   adapter: vercel(), 
-  integrations: [
-    react(),
-    keystatic()
-  ]
+  integrations: [react(), keystatic(), markdoc()]
 });
